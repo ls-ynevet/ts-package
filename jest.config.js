56c3dba6e728b05c075ce7ab10618dbca896e330
Yair Nevet <144805447+ls-ynevet@users.noreply.github.com>
2024-11-12 10:55:57 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/*.test.ts"],
+  testMatch: ["**/*.test.ts", "**/*.spec.ts"],
   moduleFileExtensions: ["ts", "js"],
 };
